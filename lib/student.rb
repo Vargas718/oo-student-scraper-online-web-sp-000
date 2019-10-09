2b32c3cae9071c171b9a49dc34162c203b2c5f52
Rafael Vargas <vargas718@gmail.com>
2019-10-09 12:43:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,8 @@ class Student
   def self.create_from_collection(students_array)
     binding.pry
     students_array.each do |students| 
-      students.collect
+      students.collect do |key, value| puts key
+                       end
                         end 
     
   
